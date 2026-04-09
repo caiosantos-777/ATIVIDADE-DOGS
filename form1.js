9d630cacf019a790eef99d5f032b7fd23fc2771c
caiox <caio_marques-santos@estudante.sesisenai.org.br>
2026-04-09 11:36:13 -0300

    add - js quase pronto

--- form1.js
+++ form1.js
@@ -1,46 +1,19 @@
 
-document.addEventListener("DOMContentLoaded", function () {
+document.getElementById("formAdocao").addEventListener("submit", function (e) {
+    e.preventDefault();
 
-    const form = document.getElementById("meuFormulario");
+    let nome = document.getElementById("Nome").value;
+    let email = document.getElementById("Email").value;
+    let moradia = document.getElementById("casa").value;
+    let quintal = document.querySelector('input[name="quintal"]:checked');
+    let cidade = document.getElementById("Cdd").value;
 
+    if (nome.length < 3) return alert("Nome Inválido");
+    if (email.length < 3) return alert("Email Inválido");
+    if (moradia.length < 3) return alert("Moradia Inválido");
+    if (quintal.length < 3) return alert("Quintal Inválido");
+    if (cidade.length < 3) return alert("Cidade Inválido");
 
-    if (!form) return;
+    document.getElementById("resultado").innerHTML = "Cadastro realizado com sucesso!<br>" + "Nome: " + nome;
 
-    form.addEventListener("submit", function (e) {
-
-        e.preventDefault();
-
-        const nome = document.getElementById("Nome").value.trim();
-        const telefone = document.getElementById("Telefone").value.trim();
-        const idadeRaw = document.getElementById("Idade").value;
-        const cidade = document.getElementById("Cdd").value.trim();
-
-
-        if (nome === "") {
-            alert("⚠️ O campo 'Nome Completo' é obrigatório.");
-            return;
-        }
-
-        if (telefone === "") {
-            alert("⚠️ O campo 'Telefone' é obrigatório.");
-            return;
-        }
-
-        if (idadeRaw === "") {
-            alert("⚠️ O campo 'Idade' é obrigatório.");
-            return;
-        } else if (Number(idadeRaw) < 16) {
-            alert("❌ Desculpe, a idade mínima para doação é 16 anos.");
-            return;
-        }
-
-        if (cidade === "") {
-            alert("⚠️ O campo 'Cidade' é obrigatório.");
-            return;
-        }
-
-        alert("✅ Tudo pronto! Seu formulário foi preenchido corretamente.");
-
-        this.submit();
-    });
 });
